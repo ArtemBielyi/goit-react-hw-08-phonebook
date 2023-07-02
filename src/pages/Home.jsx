1c983d3ffb5358b4ef9f4f07d9741b3a7f46b65b
ArtemBielyi <kolhozreshaet@gmail.com>
2023-07-02 13:39:49 +0300

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <main>
+      <section className="section">
+        <div className="container home">
+          <h1 className="home__title">Welcome to Phonebook!</h1>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Home;
---
+++ src/redux/contacts/selectors.js
@@ -0,0 +1,5 @@
+export const getFilter = state => state.filter;
+
+export const getContacts = state => state.contacts.contactList;
+export const getIsLoading = state => state.contacts.isLoading;
+export const getError = state => state.contacts.error;
